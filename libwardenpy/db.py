c05d08da44202368ce025f31fd9466fe8bcbe4f5
deepattic <148025215+deepattic@users.noreply.github.com>
2024-11-23 18:28:10 +0530

    create gloable sqlite conn

---
+++ libwardenpy/db.py
@@ -0,0 +1,13 @@
+import sqlite3
+from contextlib import contextmanager
+
+
+### This just create a sqlite connection to pass around the other functions
+### https://stackoverflow.com/questions/67436362/decorator-for-sqlite3/67436763#67436763
+@contextmanager
+def get_connection(DB_PATH="data/db.sqlite3"):
+    conn = sqlite3.connect(DB_PATH)
+    try:
+        yield conn
+    finally:
+        conn.close()
