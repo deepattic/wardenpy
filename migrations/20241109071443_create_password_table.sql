da9b463f01327b9e4aab57b0a760ea175cf31170
deepattic <148025215+deepattic@users.noreply.github.com>
2024-11-26 11:59:37 +0530

    remove manual implementations and remove migration and add it in to db

--- migrations/20241109071443_create_password_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS passwords (
-	id INTEGER PRIMARY KEY,
-	username TEXT NOT NULL,
-	site TEXT NOT NULL,
-	encrypted_password BLOB NOT NULL,
-	nonce BLOB NOT NULL,
-	FOREIGN KEY (username) REFERENCES users(username)
-);
