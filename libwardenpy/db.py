48da3d7804beae3a084bc1af052ff43ec00e5d39
deepattic <148025215+deepattic@users.noreply.github.com>
2024-11-26 12:00:04 +0530

    add migrations here

--- libwardenpy/db.py
+++ libwardenpy/db.py
@@ -8,11 +8,33 @@ from contextlib import contextmanager
 def get_connection(DB_PATH="data/db.sqlite3"):
     conn = sqlite3.connect(DB_PATH)
     try:
+        conn.execute(
+            """
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password_hash TEXT NOT NULL,
+            salt BLOB NOT NULL)
+            """
+        )
+        conn.execute(
+            """
+        CREATE TABLE IF NOT EXISTS passwords (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            site TEXT NOT NULL,
+            encrypted_password BLOB NOT NULL,
+            nonce BLOB NOT NULL,
+            FOREIGN KEY (username) REFERENCES users(username))
+            """
+        )
         yield conn
+
     except Exception as e:
         conn.rollback()
         raise e
+
     else:
         conn.commit()
+
     finally:
         conn.close()
